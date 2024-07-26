2b49f03673cab4dde2dbe840ec55c5c984a75c98
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2024-07-26 13:34:26 +0200

    ID unico para cada comida o ejercicio guardado

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,20 +2,23 @@ import { categories } from "../data/categories";
 import { useState, ChangeEvent, FormEvent, Dispatch } from "react";
 import { Activity } from "../types";
 import { ActivityActions } from "../reducers/activity-reducer";
+import { v4 as uuidv4} from 'uuid'
 
 type FormProps = {
   dispatch: Dispatch<ActivityActions>
 }
 
+const initialState : Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: '',
+  calories: 0 
+}
+
 export default function Form({dispatch} : FormProps) {
 
-  const [activity, setActivity] = useState<Activity>({
-    category: 1,
-    name: '',
-    calories: 0 
-  })
-
-
+  const [activity, setActivity] = useState<Activity>(initialState)
+  
   const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
 
     const isNumberField = ['category', 'calories'].includes(e.target.id)
@@ -35,6 +38,10 @@ export default function Form({dispatch} : FormProps) {
     e.preventDefault()
 
     dispatch({type: 'save-activity', payload: {newActivity: activity}})
+    setActivity({
+      ...initialState,
+      id: uuidv4()
+    })
   }
  
   return (
