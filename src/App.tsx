4329c654bc8f94e09f4c7412161b61713ff29fee
Sonia Moreno <94900792+Soniaa1995@users.noreply.github.com>
2024-07-27 12:09:33 +0200

    boton de reset + funcionalidad

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Form from "./components/Form"
-import { useReducer, useEffect } from "react"
+import { useReducer, useEffect, useMemo } from "react"
 import { activityReducer, initialState } from "./reducers/activity-reducer"
 import ActivityList from "./components/ActivityList"
 
@@ -11,12 +11,20 @@ function App() {
   useEffect(() => {
     localStorage.setItem('activities', JSON.stringify(state.activities))
   }, [state.activities])
+
+  const restartApp = () => useMemo(() =>state.activities.length, [state.activities])
   
   return (
     <>
       <header className="bg-lime-600 py-3">
         <div className="max-w-4xl mx-auto flex justify-between">
           <h1 className="text-center text-lg font-bold text-white uppercase">Contador de calorias</h1>
+          <button className="bg-gray-800 hover:bg-yellow-300 p-2 font-bold uppercase text-white cursor-pointer 
+                            rounded-lg text-lg disabled:opacity-10"
+                  disabled={!restartApp()}
+                  onClick={() => dispatch({type: 'restart-app'})}>
+            Reiniciar App
+          </button>
         </div>
       </header>
 
